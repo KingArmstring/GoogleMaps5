f032a293132fee36a99b08e56b0d07fe68459845
Mohammad Elsayed <m.elsayed.hamouda@gmail.com>
2017-11-15 09:42:35 -0800

    Second Commit by cppgarbagecollector: normal saving

--- GoogleMaps5/app/src/main/java/com/by_mohammadelsayed/pinpoper/googlemaps5/MainActivity.java
+++ GoogleMaps5/app/src/main/java/com/by_mohammadelsayed/pinpoper/googlemaps5/MainActivity.java
@@ -1,52 +1,185 @@
 package com.by_mohammadelsayed.pinpoper.googlemaps5;
 
 import android.*;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.IntentSender;
 import android.content.pm.PackageManager;
+import android.location.Location;
+import android.net.wifi.WifiManager;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResult;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 
-public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
     private GoogleMap mMap;
     private Marker mMarker;
     private LatLng mLatLng;
+    private WifiManager wifiManager;
+    private AlertDialog dialog;
+    private LocationRequest mLocationRequest;
+    private GoogleApiClient mGoogleApiClient;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         int permission = ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION);
-        if(permission != PackageManager.PERMISSION_GRANTED){
+        if (permission != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(MainActivity.this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, 100);
         }
 
         //TODO
         /*
-                1.upload this app using GIT
                 2.make sure that wifi and GPS are on
                 3.make sure that device connected to network
          */
 
+        wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+        if (!wifiManager.isWifiEnabled()) {
+            AlertDialog.Builder mBuilder = new AlertDialog.Builder(MainActivity.this);
+            mBuilder.setTitle("Turn Wifi On");
+            mBuilder.setMessage("This app requires Wifi to be on");
+            mBuilder.setIcon(R.mipmap.wifi_icon);
+            mBuilder.setCancelable(false);
+            mBuilder.setPositiveButton("Turn On", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    wifiManager.setWifiEnabled(true);
+                    dialog.dismiss();
+                }
+            });
+            mBuilder.setNegativeButton("Close App", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    dialog.dismiss();
+                    try {
+                        Thread.sleep(200);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    finish();
+                }
+            });
+            dialog = mBuilder.create();
+            dialog.show();
+        }
+
+        if (mGoogleApiClient == null) {
+            mGoogleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+        }
+
+        createLocationRequest();
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
+                .addLocationRequest(mLocationRequest);
+        Log.d("Armstring", "onCreate 1");
+        final PendingResult<LocationSettingsResult> result =//this is a pending intent that is triggered whenever LocationServices
+                LocationServices.SettingsApi.checkLocationSettings(mGoogleApiClient, // is used.
+                        builder.build());
+
+        Log.d("Armstring", "onCreate 2");
+        result.setResultCallback(
+                new ResultCallback<LocationSettingsResult>() {
+                    @Override
+                    public void onResult(@NonNull LocationSettingsResult locationSettingsResult) {
+                        final Status status = locationSettingsResult.getStatus();
+                        switch (status.getStatusCode()) {
+                            case LocationSettingsStatusCodes.SUCCESS:
+                                Log.d("Armstring", "Satisfied");
+                                break;
+                            case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
+                                Log.d("Armstring", "Not Satisfied");
+                                try {
+                                    status.startResolutionForResult(MainActivity.this, 100);//this method gives the user dialog
+                                } catch (IntentSender.SendIntentException e) {
+                                    e.printStackTrace();
+                                }
+                                break;
+                            case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
+                                Log.d("Armstring", "Not Satisfied can't be fixed");
+                                break;
+                        }
+                    }
+                }
+        );
+
+        Log.d("Armstring", "onCreate 3");
         MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.fragmentId);
         mapFragment.getMapAsync(this);
     }
-
 
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
         int permission = ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION);
-        if(permission != PackageManager.PERMISSION_GRANTED){
+        if (permission != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(MainActivity.this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, 100);
         }
         mMap.setMyLocationEnabled(true);
+    }
+
+    protected void createLocationRequest() {
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(3000);
+        mLocationRequest.setFastestInterval(2000);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mGoogleApiClient.disconnect();
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) !=
+                PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        Location l = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
     }
 }
